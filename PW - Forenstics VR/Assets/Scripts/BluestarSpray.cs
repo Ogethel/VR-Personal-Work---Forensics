8e0eafa55123f13c7083633bb19b0391054e0cc8
McKade <10718916@my.uvu.edu>
2020-12-17 09:29:53 -0700

    Recording Final Semester Video

---
+++ PW - Forenstics VR/Assets/Scripts/BluestarSpray.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BluestarSpray : MonoBehaviour
+{
+    public float speed = 5;
+    public GameObject waterDroplet;
+    public Transform nozzel;
+    // public AudioSource audioSource;
+    // public AudioClip audioClip;
+
+    public void Spray()
+    {
+        GameObject spawnedDroplet = Instantiate(waterDroplet, nozzel.position, nozzel.rotation);
+        spawnedDroplet.GetComponent<Rigidbody>().velocity = speed * nozzel.forward;
+        Destroy(spawnedDroplet, 2);
+    }
+}
---
+++ PW - Forenstics VR/Assets/Scripts/WaterDroplets.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterDroplets : MonoBehaviour
+{
+
+    public Transform spawnPoint;
+    public GameObject waterTexture;
+    public LayerMask canHit;
+    public float offsetVal = .5f;
+    private bool hasAlreadyCollided = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!hasAlreadyCollided)
+        {
+            if ((canHit.value & 1<<other.gameObject.layer) ==1<<other.gameObject.layer)
+            {
+                hasAlreadyCollided = true;
+                Vector3 newInsTransform = new Vector3(0, offsetVal, 0);
+                GameObject newTexture;
+                newTexture = Instantiate(waterTexture, this.transform.position + newInsTransform, Quaternion.identity);
+            }
+        }
+    }
+
+}
